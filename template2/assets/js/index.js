2d4191e575c34bf27c3609b8153269cdcc6f08aa
yangeunjin <jinny920311@gmail.com>
2020-08-11 18:59:31 +0900

    템플릿 2(회사페이지) 제작중

---
+++ template2/assets/js/index.js
@@ -0,0 +1,37 @@
+/*
+ * Copyright (c) 2020
+ * Created by yangeunjin
+ * Github: https://github.com/jinnny
+ */
+
+$(document).ready(function() {
+  $('.input-content .input-content__input').focus(function () {
+    $(this).siblings('.input-content__label').addClass('is--active')
+    $(this).parent('.input-content').addClass('is--active')
+  }).blur(function() {
+    if ($(this).val() === '') {
+      $(this).siblings('.input-content__label').removeClass('is--active')
+      $(this).parent('.input-content').removeClass('is--active')
+    }
+  });
+  const mySwiper = new Swiper('.swiper-container', {
+    // Optional parameters
+    loop: true,
+    effect: 'fade',
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    }
+  })
+
+  $(window).scroll(function(){
+    let max = 1000; //100% 투명할때의 스크롤 값
+    let scrollPX = $(this).scrollTop();
+    console.log(scrollPX)
+    if( scrollPX  < max ) {
+      $("#bg").css({"opacity": (max-scrollPX)/max });
+    }else{
+      $("#bg").css({"opacity": 0});
+    }
+  });
+});
